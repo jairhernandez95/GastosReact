d784e8f0c129eb01a143d20e041204a4f8803a59
Jair Hernández <jair_097@hotmail.com>
2022-06-06 19:27:45 -0700

    Update

--- gastosReact/src/components/IngresosEgresos.jsx
+++ gastosReact/src/components/IngresosEgresos.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalContext'
+
 
 const IngresosEgresos = () => {
+
+  const { transacciones } = useContext(GlobalContext)
+  const importes = transacciones.map(transaccion => transaccion.importe)
+
+  const ingresos =  importes
+    .filter(item => item > 0)
+    .reduce((acumulador, item) => (acumulador += item), 0)
+    .toFixed(2)
+
+  const egresos = importes
+    .filter(item => item < 0)
+    .reduce((acumulador, item) => (acumulador += item), 0) * -1
+    .toFixed(2)
+
   return (
     <div className='inc-exp-container'>
       <div>
       <h4>Ingresos</h4>
-      <p className='money plus'>+$0.00</p>
+      <p className='money plus'>+${ingresos}</p>
     </div>
     <div>
       <h4>Egresos</h4>
-      <p className='money minus'>+$0.00</p>
+      <p className='money minus'>-${egresos}</p>
     </div>
     </div>
 
